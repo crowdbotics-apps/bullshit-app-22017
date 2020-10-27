a873ce7d37316e97d7bd0961e08d607e3b4d9d83
Crowdbotics <team@crowdbotics.com>
2020-10-27 18:31:05 +0000

    Initial commit.

---
+++ bullshit_app_22017/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for bullshit_app_22017 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bullshit_app_22017.settings')
+
+application = get_wsgi_application()
